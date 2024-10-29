6d727edfff0b8b43bbdea72188b84cbdc77d2922
Division7 <djensen2@outlook.com>
2024-10-29 03:57:15 -0700

    Added firebase functionality, confirmed via authtest app

--- GOLDTracker/src/components/ClassRegister.js
+++ GOLDTracker/src/components/ClassRegister.js
@@ -1,4 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {auth, database} from "@/firebaseConfig"
+import {ref,get,set, child} from "firebase/database"
 export async function registerClass(courseId, sectionId){
     let classList = await AsyncStorage.getItem("class-list");
     if(!classList){
@@ -43,10 +45,81 @@ export async function deregisterClass(courseId, sectionId){
 
 export async function getClasses(){
     let classList = await AsyncStorage.getItem("class-list");
-
+    console.log(await AsyncStorage.getItem("timestamp"));
     if(!classList){
         return null;
     }else{
-        return JSON.parse(classList);
+        return classList;
     }
+
+}
+
+export async function reset(){
+    await AsyncStorage.setItem("class-list","");
+    await AsyncStorage.setItem("timestamp","");
+}
+
+export async function syncToFirebase(){
+    let classList = JSON.parse(await AsyncStorage.getItem("class-list"));
+    let timestamp = parseFloat(await AsyncStorage.getItem("timestamp"));
+
+    const loc = ref(database)
+    let serverClassList = await get(child(loc, `users/${auth.currentUser.uid}/classList`)).then((current) => {
+        if(current.exists()){
+            return JSON.parse(current.val())
+        }else{
+            return null;
+        }
+    })
+
+
+    let serverTimestamp = await get(child(loc, `users/${auth.currentUser.uid}/timestamp`)).then((current) => {
+        if(current.exists()){
+            return current.val()
+        }else{
+
+            return null;
+        }
+    })
+
+
+    if(!serverTimestamp && ! timestamp){
+        return;
+    }
+
+    else if(!serverTimestamp){
+        updateServer(classList, timestamp);
+    }
+
+    else if(!timestamp){
+        await updateLocal(serverClassList,serverTimestamp)
+    }
+
+    else if (timestamp > serverTimestamp){
+        await updateServer(classList, serverTimestamp);
+    }
+
+    else if(timestamp < serverTimestamp){
+        await updateLocal(serverClassList,serverTimestamp)
+    }
+
+    else{
+        //are the same.
+        return;
+    }
+
+}
+
+async function updateServer(classList, timestamp){
+    const loc = ref(database, `users/${auth.currentUser.uid}`)
+    await set(loc,{
+        classList: JSON.stringify(classList),
+        timestamp: timestamp
+    });
+    console.log("this ran")
+}
+
+async function updateLocal(classList, timestamp){
+    await AsyncStorage.setItem("timestamp", `${timestamp}`);
+    await AsyncStorage.setItem("class-list", JSON.stringify(classList));
 }
