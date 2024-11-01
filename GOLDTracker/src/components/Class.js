dc0717880bc0e6d390dbb6b03bd8a3af78446cc8
xinyao-song <xinyaosong@ucsb.edu>
2024-10-31 23:39:17 -0700

    xs-fully fixed UI and fuctionality of search

--- GOLDTracker/src/components/Class.js
+++ GOLDTracker/src/components/Class.js
@@ -1,58 +1,107 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { COLORS, SPACING } from "../theme/theme";
-import Entypo from "@expo/vector-icons/Entypo"; 
+import Entypo from "@expo/vector-icons/Entypo";
 
 class Class extends React.Component {
-    goToDetails = () => {
-        const { course } = this.props;
-        this.props.navigation.navigate("CourseDetailsScreen", { course });
-      };
-    
+  goToDetails = () => {
+    const { course } = this.props;
+    this.props.navigation.navigate("CourseDetailsScreen", { course });
+  };
 
   render() {
     const { course, toggleFollow } = this.props;
 
+    const courseCode = course.courseId ? course.courseId.trim() : "N/A";
+    const courseTitle = course.title || "No Title";
+    const firstSection = course.classSections && course.classSections[0];
+
+    const courseTime =
+      firstSection &&
+      firstSection.timeLocations &&
+      firstSection.timeLocations[0]
+        ? `${firstSection.timeLocations[0].beginTime} - ${firstSection.timeLocations[0].endTime}`
+        : "N/A";
+
+    const courseProfessor =
+      firstSection && firstSection.instructors && firstSection.instructors[0]
+        ? firstSection.instructors[0].instructor
+        : "N/A";
+
     return (
       <View style={styles.wrapper}>
-        {/* Course Name */}
-        <Text style={styles.courseCode}>{course.code}</Text>
+        {/* Course Code */}
+        <Text style={styles.courseCode}>{courseCode}</Text>
 
         <View style={styles.courseContainer}>
-         
+          {/* Course Title */}
+          <Text style={styles.courseTitle}>{courseTitle}</Text>
+
           <View style={styles.courseDetailsContainer}>
-            <Text style={styles.courseTime}>{course.time}</Text>
+            <Text style={styles.courseTime}>{courseTime}</Text>
             <View style={styles.professorContainer}>
-              <Text style={styles.courseProfessor}>{course.professor}</Text>
-              
-              {/* TODO: Toggle to CourseDetialScreen */}
-              <TouchableOpacity onPress={this.goToDetails} style={styles.detailsButton}>
+              <Text style={styles.courseProfessor}>{courseProfessor}</Text>
+
+              {/* Navigate to CourseDetailsScreen */}
+              {/* TODO */}
+              <TouchableOpacity
+                onPress={this.goToDetails}
+                style={styles.detailsButton}
+              >
                 <Entypo name="chevron-right" size={16} color={COLORS.black} />
               </TouchableOpacity>
             </View>
           </View>
 
-          {course.sections.map((section, index) => (
-            <View
-              key={section.id}
-              style={[
-                styles.sectionContainer,
-                { backgroundColor: index % 2 === 0 ? COLORS.lightGrey : COLORS.darkGrey }
-              ]}
-            >
-              <Text style={styles.sectionTime}>{section.time}</Text>
-              <Text style={styles.sectionSpace}>Space: {section.space}</Text>
-              <TouchableOpacity
-                style={[
-                  styles.followButton,
-                  section.following ? styles.following : styles.notFollowing
-                ]}
-                onPress={() => toggleFollow(course.id, section.id)}
-              >
-                <Text style={styles.followText}>{section.following ? "Following" : "Follow"}</Text>
-              </TouchableOpacity>
-            </View>
-          ))}
+          {course.classSections &&
+            course.classSections.map((section, index) => {
+              // Section Time
+              const sectionTime =
+                section.timeLocations && section.timeLocations[0]
+                  ? `${section.timeLocations[0].beginTime} - ${section.timeLocations[0].endTime}`
+                  : "N/A";
+
+              // Section Space
+              const sectionSpace = `${section.enrolledTotal || 0}/${
+                section.maxEnroll || 0
+              }`;
+
+              // Ensure 'following' property exists
+              if (section.following === undefined) {
+                section.following = false;
+              }
+
+              return (
+                <View
+                  key={section.enrollCode}
+                  style={[
+                    styles.sectionContainer,
+                    {
+                      backgroundColor:
+                        index % 2 === 0 ? COLORS.lightGrey : COLORS.darkGrey,
+                    },
+                  ]}
+                >
+                  <Text style={styles.sectionTime}>{sectionTime}</Text>
+                  <Text style={styles.sectionSpace}>Space: {sectionSpace}</Text>
+                  <TouchableOpacity
+                    style={[
+                      styles.followButton,
+                      section.following
+                        ? styles.following
+                        : styles.notFollowing,
+                    ]}
+                    onPress={() =>
+                      toggleFollow(course.courseId.trim(), section.section)
+                    }
+                  >
+                    <Text style={styles.followText}>
+                      {section.following ? "Following" : "Follow"}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              );
+            })}
         </View>
       </View>
     );
@@ -62,36 +111,42 @@ class Class extends React.Component {
 const styles = StyleSheet.create({
   wrapper: {
     marginBottom: SPACING.space_8,
-    paddingTop: SPACING.space_10, 
+    paddingTop: SPACING.space_10,
     paddingHorizontal: SPACING.space_8,
-    paddingLeft: SPACING.space_12, 
+    paddingLeft: SPACING.space_12,
+    margin: SPACING.space_8,
   },
   courseContainer: {
     backgroundColor: COLORS.darkGrey,
     padding: SPACING.space_8,
     borderRadius: 8,
-    marginTop: SPACING.space_18, 
+    marginTop: SPACING.space_18,
   },
   courseCode: {
     fontSize: 18,
     fontWeight: "bold",
     color: COLORS.ucsbBlue,
     position: "absolute",
-    top: SPACING.space_4, 
+    top: SPACING.space_4,
     left: SPACING.space_2,
     backgroundColor: COLORS.lightYellow,
     paddingVertical: SPACING.space_4,
     paddingHorizontal: SPACING.space_4,
     borderRadius: 8,
     overflow: "hidden",
-    zIndex: 1, 
+    zIndex: 1,
+  },
+  courseTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: COLORS.black,
+    marginBottom: SPACING.space_4,
   },
   courseDetailsContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     marginBottom: SPACING.space_4,
-    marginTop: SPACING.space_4,
   },
   professorContainer: {
     flexDirection: "row",
@@ -100,12 +155,12 @@ const styles = StyleSheet.create({
   courseTime: {
     fontSize: 14,
     color: COLORS.black,
-    flex: 1, 
+    flex: 1,
   },
   courseProfessor: {
     fontSize: 14,
     color: COLORS.black,
-    marginRight: SPACING.space_6, 
+    marginRight: SPACING.space_6,
   },
   detailsButton: {
     padding: SPACING.space_,
--- GOLDTracker/src/screen/HomeScreen.js
+++ GOLDTracker/src/screen/HomeScreen.js
@@ -1,41 +1,20 @@
 import React, { useState } from "react";
-import { View, ScrollView, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import {
+  View,
+  ScrollView,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
 import { COLORS, SPACING } from "../theme/theme";
-import Class from "../components/Class";
-import coursesData from "../assets/courses"; // replace later with school API
-import SearchComponent from '../components/SearchComponent';
+import SearchComponent from "../components/SearchComponent";
 
 const HomeScreen = ({ navigation }) => {
-  const [courses, setCourses] = useState(coursesData);
-
-  // TODO: Toggle follow status of a section, following status will be passed to cart in later implimentation
-  const toggleFollow = (courseId, sectionId) => {
-    setCourses((prevCourses) =>
-      prevCourses.map((course) =>
-        course.id === courseId
-          ? {
-              ...course,
-              sections: course.sections.map((section) =>
-                section.id === sectionId
-                  ? { ...section, following: !section.following }
-                  : section
-              ),
-            }
-          : course
-      )
-    );
-  };
-
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <ScrollView contentContainerStyle={styles.scrollContent}>
         <SearchComponent />
-          {courses.map((course) => (
-            <Class key={course.id} course={course} toggleFollow={toggleFollow} navigation={navigation}/>
-          ))}
-        </ScrollView>
       </View>
     </SafeAreaView>
   );
