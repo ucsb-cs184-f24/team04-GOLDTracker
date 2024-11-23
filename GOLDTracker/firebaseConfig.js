52d1ce2fb12c65fec8b1796f44a1dc004d55d3e8
Zhen Bi <zhenbi@csil.cs.ucsb.edu>
2024-11-23 00:25:38 -0800

    firebaseConfig repeated code delete

--- GOLDTracker/firebaseConfig.js
+++ GOLDTracker/firebaseConfig.js
@@ -1,6 +1,5 @@
 import {initializeApp} from "firebase/app";
 import {getDatabase} from "firebase/database";
-import { getFirestore} from "firebase/firestore";
 import {
     FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN,
@@ -29,8 +28,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
-
-export const firestore = getFirestore(app);
 
 export const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
