072c1e348fa8c8f7b3e9ed1c0ff1c491ba52643b
Zhen Bi <zhenbi@csil.cs.ucsb.edu>
2024-12-03 00:47:17 -0800

    overlay

--- GOLDTracker/src/components/SearchComponent.js
+++ GOLDTracker/src/components/SearchComponent.js
@@ -1,10 +1,24 @@
-import React, { useState, useEffect, useRef } from "react";
-import { StatusBar, StyleSheet, View, FlatList, Text,ActivityIndicator } from "react-native";
+import React, { useState, useEffect, useRef, forwardRef, useImperativeHandle, } from "react";
+import { 
+  StatusBar, 
+  StyleSheet, 
+  View, 
+  FlatList, 
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+  Animated,
+  Easing,
+  TouchableWithoutFeedback,
+ } from "react-native";
 import { SearchBar } from "react-native-elements";
 import Class from "../components/Class";
 import { useNavigation } from "@react-navigation/native";
 import { auth } from "../../firebaseConfig";
-import CategorySearch from "../components/CategorySearch";
+import { COLORS } from "../theme/theme";
+import departmentMapping from "../assets/departmentMapping.json";
+import AntDesign from "react-native-vector-icons/AntDesign";
+
 
 const SearchComponent = ({ search, setSearch, setIsSearching, major }) => {
   const [results, setResults] = useState([]);
@@ -12,6 +26,26 @@ const SearchComponent = ({ search, setSearch, setIsSearching, major }) => {
   const [selectedDept, setSelectedDept] = useState(null);
   const [selectedQuarter, setSelectedQuarter] = useState("20244");
   const [isLoading, setIsLoading] = useState(false);
+  const [isDeptDropdownVisible, setIsDeptDropdownVisible] = useState(false);
+  const [isQuarterDropdownVisible, setIsQuarterDropdownVisible] =
+    useState(false);
+  const animatedDeptHeight = useRef(new Animated.Value(0)).current;
+  const animatedQuarterHeight = useRef(new Animated.Value(0)).current;
+
+  const departmentOptions = [
+    { code: "Your Major", label: "Cancel Selection" }, // Add "My Major" option
+    ...Object.keys(departmentMapping).map((code) => ({
+      code,
+      label: departmentMapping[code][0],
+    })),
+  ];
+
+  const quarterOptions = [
+    { code: "20244", label: "Fall 2024" },
+    { code: "20243", label: "Summer 2024" },
+    { code: "20242", label: "Spring 2024" },
+    { code: "20241", label: "Winter 2024" },
+  ];
 
   const categorySearchRef = useRef(null);
   const navigation = useNavigation();
@@ -38,6 +72,93 @@ const SearchComponent = ({ search, setSearch, setIsSearching, major }) => {
       )
     );
   };
+  const toggleDeptDropdown = () => {
+    if (!isDeptDropdownVisible) {
+      setIsSearching(true);
+    } else if (!isQuarterDropdownVisible) {
+      setIsSearching(false);
+    }
+    setIsDeptDropdownVisible((prev) => !prev);
+    if (!isDeptDropdownVisible) {
+      setIsQuarterDropdownVisible(false);
+      Animated.timing(animatedQuarterHeight, {
+        toValue: 0,
+        duration: 0,
+        useNativeDriver: false,
+      }).start();
+    }
+    Animated.timing(animatedDeptHeight, {
+      toValue: isDeptDropdownVisible ? 0 : 300,
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const toggleQuarterDropdown = () => {
+    if (!isQuarterDropdownVisible) {
+      setIsSearching(true);
+    } else if (!isDeptDropdownVisible) {
+      setIsSearching(false);
+    }
+    setIsQuarterDropdownVisible((prev) => !prev);
+    if (!isQuarterDropdownVisible) {
+      setIsDeptDropdownVisible(false);
+      Animated.timing(animatedDeptHeight, {
+        toValue: 0,
+        duration: 0,
+        useNativeDriver: false,
+      }).start();
+    }
+    Animated.timing(animatedQuarterHeight, {
+      toValue: isQuarterDropdownVisible ? 0 : 300,
+      duration: 300,
+      easing: Easing.inOut(Easing.ease),
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const handleSelect = (type, item) => {
+    if (type === "department") {
+      if (item.code === "Your Major") {
+        setSelectedDept(major); // Set to the user's major
+        handleSearchSubmit(major); // Trigger search with major
+      } else {
+        setSelectedDept(item.code);
+        handleSearchSubmit(item.code);
+      }
+      toggleDeptDropdown();
+    } else if (type === "quarter") {
+      setSelectedQuarter(item.code);
+      toggleQuarterDropdown();
+    }
+  };
+
+  const closeDropdowns = () => {
+    if (isDeptDropdownVisible || isQuarterDropdownVisible) {
+      setIsSearching(false);
+    }
+    if (isDeptDropdownVisible) {
+      setIsDeptDropdownVisible(false);
+      Animated.timing(animatedDeptHeight, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    }
+    if (isQuarterDropdownVisible) {
+      setIsQuarterDropdownVisible(false);
+      Animated.timing(animatedQuarterHeight, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    }
+  };
+
+  useImperativeHandle(categorySearchRef, () => ({
+    closeDropdowns,
+  }));
 
   const handleSearchSubmit = async (deptCode = null) => {
     if (major && major !== "") {
@@ -143,21 +264,112 @@ const SearchComponent = ({ search, setSearch, setIsSearching, major }) => {
         onClear={handleClearSearch}
       />
 
-      {/* Category Search Dropdown */}
-      <CategorySearch
-        ref={categorySearchRef}
-        onDepartmentSelect={setSelectedDept}
-        onQuarterSelect={setSelectedQuarter}
-        selectedDept={selectedDept}
-        onSearch={handleSearchSubmit}
-        selectedQuarter={selectedQuarter}
-        setIsSearching={setIsSearching} // Pass the setter function
-        major = {major}
-      />
+      {/*filters */}
+      <View style={styles.buttonRow}>
+      <TouchableOpacity
+            style={[styles.toggleButton, styles.centeredButton]}
+            onPress={toggleDeptDropdown}
+      >
+      <Text style={styles.toggleButtonText}>
+        {selectedDept || "Department"}
+            </Text>
+            <AntDesign
+              name={isDeptDropdownVisible ? "caretup" : "caretdown"}
+              size={12}
+              color="#fff"
+              style={styles.shadowIcon}
+            />
+          </TouchableOpacity>
 
-    {isLoading ? (
-      <ActivityIndicator size={25} color="#0000ff" />
-    ) : null}
+          <TouchableOpacity
+            style={[styles.toggleButton, styles.centeredButton]}
+            onPress={toggleQuarterDropdown}
+          >
+            <Text style={styles.toggleButtonText}>
+              {selectedQuarter
+              ? `${selectedQuarter.slice(0, 4)} ${
+                  quarterOptions.find((q) => q.code === selectedQuarter)?.label[0]
+                }`
+              : "Select Quarter"}
+            </Text>
+            <AntDesign
+              name={isQuarterDropdownVisible ? "caretup" : "caretdown"}
+              size={12}
+              color="#fff"
+              style={styles.shadowIcon}
+            />
+          </TouchableOpacity>
+        </View>
+
+        {(isDeptDropdownVisible || isQuarterDropdownVisible) && (
+          <TouchableWithoutFeedback onPress={closeDropdowns}>
+            <View style={styles.overlay}>
+              {isDeptDropdownVisible && (
+                <Animated.View
+                  style={[
+                    styles.dropdownContainer,
+                    { height: animatedDeptHeight },
+                  ]}
+                >
+                  <FlatList
+                    data={departmentOptions}
+                    keyExtractor={(item) => item.code}
+                    renderItem={({ item }) => (
+                      <TouchableOpacity
+                        style={styles.dropdownOption}
+                        onPress={() => handleSelect("department", item)}
+                      >
+                        <Text
+                          style={[
+                            styles.dropdownOptionText,
+                            selectedDept === item.code && styles.selectedText,
+                          ]}
+                        >
+                          {item.label} ({item.code})
+                        </Text>
+                      </TouchableOpacity>
+                    )}
+                    contentContainerStyle={{ paddingBottom: 20 }}
+                  />
+                </Animated.View>
+              )}
+
+              {isQuarterDropdownVisible && (
+                <Animated.View
+                  style={[
+                    styles.dropdownContainer,
+                    { height: animatedQuarterHeight },
+                  ]}
+                >
+                  <FlatList
+                    data={quarterOptions}
+                    keyExtractor={(item) => item.code}
+                    renderItem={({ item }) => (
+                      <TouchableOpacity
+                        style={styles.dropdownOption}
+                        onPress={() => handleSelect("quarter", item)}
+                      >
+                        <Text
+                          style={[
+                            styles.dropdownOptionText,
+                            selectedQuarter === item.code && styles.selectedText,
+                          ]}
+                        >
+                          {item.label}
+                        </Text>
+                      </TouchableOpacity>
+                    )}
+                    contentContainerStyle={{ paddingBottom: 20 }}
+                  />
+                </Animated.View>
+              )}
+            </View>
+          </TouchableWithoutFeedback>
+        )}
+
+      {isLoading ? (
+        <ActivityIndicator size={25} color="#0000ff" />
+      ) : null}
 
       {/* Error Message or Results */}
       {errorMessage ? (
@@ -188,6 +400,7 @@ const SearchComponent = ({ search, setSearch, setIsSearching, major }) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
+    flex:1,
   },
   searchBarContainer: {
     width: "95%",
@@ -211,13 +424,85 @@ const styles = StyleSheet.create({
     color: "#d9534f",
     textAlign: "center",
   },
+  overlay: {
+    position: "absolute",
+    top: 130,  // Start at the top of the screen/container
+    left: 0,  // Align with the left side of the screen
+    right: 0,  // Align with the right side of the screen
+    bottom: 0,  // Cover everything below the header (up to the course list)
+    backgroundColor: "rgba(0, 0, 0, 0.3)",  // Semi-transparent grey overlay
+    zIndex: 1,  // Ensure it is above the course list and other elements
+  },
   flatList: {
     marginTop: 20, // Space between search bar and first course
   },
   leftIconContainer: {
     marginLeft: 15, // Adjust the value to move the icon further to the right
   },
-  
+  buttonRow: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  toggleButton: {
+    backgroundColor: COLORS.darkBlue,
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    borderRadius: 12,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 18,
+    width: "40%",
+  },
+  centeredButton: {
+    justifyContent: "center",
+  },
+  toggleButtonText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingRight: 5,
+    fontFamily: "Nunito-Regular", 
+  },
+  dropdownContainer: {
+    backgroundColor: "#fff",
+    overflow: "hidden",
+    borderRadius: 5, // Outer curve for the border
+    marginHorizontal: 5,
+    marginTop: 10,
+    borderBottomWidth: 8, 
+    borderBottomColor: COLORS.orange, 
+    shadowColor: "#000", 
+    shadowOffset: { width: 0, height: 4 }, 
+    shadowOpacity: 0.2, 
+    shadowRadius: 6, 
+    elevation: 5,
+  },
+  dropdownOption: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  dropdownOptionText: {
+    fontSize: 16,
+    color: "#333",
+    fontFamily: "Nunito-Regular", 
+  },
+  selectedText: {
+    fontWeight: "bold",
+    color: COLORS.orange,
+    fontFamily: "Nunito-Regular", 
+  },
+  shadowIcon: {
+    shadowColor: "#000",
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 5, // Shadow for Android
+  },
 });
 
 export default SearchComponent;
